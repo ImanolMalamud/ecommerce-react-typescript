e825ec7b2c0b866ed92b6f81c0ec83ee36895317
ImanolMalamud <imanolm93@gmail.com>
2022-12-26 22:41:53 -0300

    Agregue un mensaje a la pagina Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,8 @@
 export function Home() {
-    return <h1>Home</h1>
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Esta app está escrita con Typescript, React. Utiliza react-bootstrap tambien.</p>
+    </>
+  );
 }
