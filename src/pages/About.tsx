236c4eefe64dd2e2cb0ee28149486d8ba2e31937
ImanolMalamud <imanolm93@gmail.com>
2022-12-26 22:51:46 -0300

    Agregue info a las paginas Home y About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,8 @@
 export function About() {
-    return <h1>About</h1>
+  return (
+    <>
+      <h1>About</h1>
+      <p>Imanol Malamud</p>
+    </>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,9 @@ export function Home() {
   return (
     <>
       <h1>Home</h1>
-      <p>Esta app está escrita con Typescript, React. Utiliza react-bootstrap tambien.</p>
+      <p>Esta app está escrita con Typescript y React. Utiliza react-bootstrap tambien.</p>
+      <p>En la pagina Store se pueden ver algunos articulos.</p>
+      <p>Esto lo aprendi en unos dias. Vamos practicando tecnologias para el proyecto de Gamming =D</p>
     </>
   );
 }
